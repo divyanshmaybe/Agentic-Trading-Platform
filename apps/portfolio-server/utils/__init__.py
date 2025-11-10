35f74962ffcd104ebb8d1d522693ea938543f679
Abhishek-Punhani <punhani.manavabhi@gmail.com>
2025-11-10 20:44:09 +0530

    added some tests to verify
    Signed-off-by: Abhishek-Punhani <punhani.manavabhi@gmail.com>

--- apps/portfolio-server/utils/__init__.py
+++ apps/portfolio-server/utils/__init__.py
@@ -5,6 +5,7 @@ Utilities exported at the package level for convenience.
 from .backtesting import BacktestConfig, serialise_results
 from .pipeline_utils import get_pipeline_status
 from .portfolio_allocation import allocate_portfolios, prepare_allocation_requests
+from pipelines.nse.backtest_pipeline import run_backtest_pipeline  # type: ignore  # noqa: E402
 
 __all__ = [
     "BacktestConfig",
